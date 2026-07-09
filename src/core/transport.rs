09585920494121ea79086c860e2ebd6b40c4231f
DESKTOP-8BKARR2\hongjunlee <hong3883@naver.com>
2026-07-10 01:26:49 +0900

    Implement TCP connection handling and refactor transport module structure

--- src/core/transport.rs
+++ src/core/transport.rs
@@ -1,4 +1,7 @@
-mod handler;
+use std::sync::mpsc::Sender;
+
+pub(crate) mod tcp;
+pub(crate) mod udp;
 
 //
 // Transport
@@ -6,4 +9,36 @@ mod handler;
 
 pub type ConnId = u64;
 
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+pub enum TransportType {
+    Tcp = 0,
+    Udp = 1,
+}
 
+#[derive(Clone)]
+pub enum TransportCommand {
+    Send(ConnId, Vec<u8>),
+    Close(ConnId),
+    Shutdown,
+}
+
+pub struct PollerHandle {
+    pub tx: Sender<TransportCommand>,
+}
+
+impl PollerHandle {
+    pub fn send(&self, id: ConnId, data: Vec<u8>) {
+        let _ = self.tx.send(TransportCommand::Send(id, data));
+    }
+
+    pub fn close(&self, id: ConnId) {
+        let _ = self.tx.send(TransportCommand::Close(id));
+    }
+}
+
+pub trait TransportHandler: Send + Sync + 'static {
+    fn on_connect(&self, h: &PollerHandle);
+    fn on_data(&self, data: &[u8], h: &PollerHandle);
+    fn on_close(&self, h: &PollerHandle);
+}
--- src/core/transport/handler.rs
+++
@@ -1,9 +0,0 @@
-//
-// Transport Handler
-//
-
-pub trait TransportHandler: Send + Sync + 'static {
-    fn on_connect(&self);
-    fn on_data(&self, data: &[u8]);
-    fn on_close(&self);
-}
